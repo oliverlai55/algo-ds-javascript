fcc9aa37d33305b35b620f3c127064f7015089e5
Oliver Lai <oliverlai55@gmail.com>
2018-08-20 09:51:43 -0400

    Add factorial

--- recursion/fractorial/fractorial.test.js
+++ recursion/fractorial/fractorial.test.js
@@ -1,5 +1,9 @@
 const fractorial = require('./fractorial');
 
 test('fractorial of 5', () => {
-  expect(fractorial(5)).toBe(15);
+  expect(fractorial(7)).toBe(5040);
+});
+
+test('fractorial of 4', () => {
+  expect(fractorial(4)).toBe(24);
 });
